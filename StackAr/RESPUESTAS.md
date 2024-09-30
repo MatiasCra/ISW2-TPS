21e70226377304f75b09e45dfb73c40156c5417b
MatiasCra <cravchikm@gmail.com>
2024-09-30 19:31:16 -0300

    respuestas parte 1

--- StackAr/RESPUESTAS.md
+++ StackAr/RESPUESTAS.md
@@ -1,2 +1,68 @@
 ## Ej 1
-### a) 79
+
+### a)
+
+74 mutantes
+
+| Mutator              | Cantidad de mutantes |
+| -------------------- | -------------------- |
+| ConditionalsBoundary | 3                    |
+| Math                 | 5                    |
+| NegateConditions     | 9                    |
+| FalseConditionals    | 10                   |
+| TrueConditionals     | 10                   |
+| MinusOneConstant     | 8                    |
+| OneConstant          | 6                    |
+| ZeroConstant         | 5                    |
+| EmptyReturns         | 3                    |
+| FalseReturns         | 4                    |
+| NullReturns          | 2                    |
+| TrueReturns          | 6                    |
+| Increments           | 3                    |
+| Total                | 74                   |
+
+### b)
+
+Los que generaron más fueron TrueConditionals y FalseConditionals
+
+### c)
+
+El que generó menos fué NullReturns
+
+## Ej2
+
+| Test suite  | Vivos | Muertos | Score |
+| ----------- | ----- | ------- | ----- |
+| StackTests1 | 55    | 19      | 25%   |
+| StackTests2 | 38    | 36      | 48%   |
+
+## Ej 3
+
+### a)
+
+93%
+
+### b)
+
+Vivos: 5
+Muertos: 69
+
+### c)
+
+- StackArMutated6302 (FalseConditionalsMutator: Se reemplazó this == obj por false en la línea 96.)
+  El if de la linea 96 puede ganar performance pero no cambia la decicion de si se devuelve true o false ya que si los stacks son la misma variable igualmente dará true al comparar los elementos.
+- StackArMutated19 (MathMutator: Se reemplazó _ por / en la línea 89.)
+  Reemplazar _ 1 por / 1 no cambia nada
+- StackArMutated3933 (FalseConditionalsMutator: Se reemplazó isEmpty() por false en la línea 69.)
+  El chequeo se vuelve a hacer en .top()
+- StackArMutated7656 (FalseConditionalsMutator: Se reemplazó readIndex != other.readIndex por false en la línea 105.)
+  Los readIndex no pueden ser distintos si los elements son iguales, chequeo que se hace antes y si son distintos entra en otro return
+- StackArMutated7687 (TrueReturnsMutator: Se reemplazó false por true en la línea 106.)
+  Por misma razon que el anterior, nunca entra en el if
+
+### d)
+59%
+
+### e)
+StackArMutated687 (TrueConditionalsMutator: Se reemplazó capacity < 0 por true en la línea 19.)
+Este mutate hace que siempre se tire error al crear el stack por lo que no permite ejecutar el resto de instrucciones del stack
--- StackAr/src/test/java/org/autotest/StackTests3.java
+++ StackAr/src/test/java/org/autotest/StackTests3.java
@@ -1,6 +1,7 @@
 package org.autotest;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -62,4 +63,93 @@ public class StackTests3 extends MutationAnalysisRunner {
     }
 
     // COMPLETAR
+    public void testReturnedPop() throws Exception {
+        Stack stack = createStack(2);
+        stack.push(42);
+        assertEquals(42, stack.pop());
+    }
+
+    public void testNonZeroHash() throws Exception {
+        Stack stack = createStack(2);
+        stack.push(42);
+        assertNotEquals(0, stack.hashCode());
+    }
+
+    public void testEmptyStacksNotEqualsNotEmptyStack() throws Exception {
+        Stack stack1 = createStack();
+        Stack stack2 = createStack();
+        stack1.push(42);
+        assertFalse(stack1.equals(stack2));
+    }
+
+    public void testNotEmptyEqualStacks() throws Exception {
+        Stack stack1 = createStack();
+        Stack stack2 = createStack();
+        stack1.push(42);
+        stack2.push(42);
+        assertTrue(stack1.equals(stack2));
+    }
+
+    public void testStackEqualsItself() throws Exception {
+        Stack stack = createStack();
+        stack.push(42);
+        assertTrue(stack.equals(stack));
+    }
+
+    public void testDefaultCapacityIs10() throws Exception {
+        Stack stack = createStack();
+        for (int i = 0; i < 10; i++) {
+            assertDoesNotThrow(() -> stack.push(0));
+        }
+    }
+
+    public void testEmptyStackDoesNotEqualsNull() throws Exception {
+        Stack stack = createStack();
+        assertFalse(stack.equals(null));
+    }
+
+    public void testStackHasCorrectHash() throws Exception {
+        Stack stack1 = createStack();
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Arrays.hashCode(new Object[10]);
+        result = prime * result + (-1);
+        assertEquals(stack1.hashCode(), result);
+    }
+
+    public void testStackDoesNotEqualAnInt() throws Exception {
+        Stack stack = createStack();
+        assertFalse(stack.equals(1));
+    }
+
+    public void testCantGoPastCapacity() throws Exception {
+        Stack stack = createStack();
+        for (int i = 0; i < 10; i++) {
+            stack.push(0);
+        }
+
+        assertThrows(IllegalStateException.class, () -> stack.push(0));
+    }
+
+    public void testCantPopEmptyStack() throws Exception {
+        Stack stack = createStack();
+        assertThrows(IllegalStateException.class, () -> stack.pop());
+    }
+
+    public void testsDifferentStacksWithSameSizeAreNotEqual() throws Exception {
+        Stack stack1 = createStack();
+        Stack stack2 = createStack();
+        stack1.push(42);
+        stack2.push(24);
+        assertFalse(stack1.equals(stack2));
+    }
+
+    public void testCanCreateZeroCapacityStack() throws Exception {
+        assertDoesNotThrow(() -> createStack(0));
+    }
+
+    public void testCantGetTopOfEmptyStack() throws Exception {
+        Stack stack = createStack();
+        assertThrows(IllegalStateException.class, () -> stack.top());
+    }
 }
