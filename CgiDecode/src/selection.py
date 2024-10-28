92d2323990729c1da099a4becfe101a7bd350752
MatiasCra <cravchikm@gmail.com>
2024-10-28 17:39:20 -0300

    ej7

--- CgiDecode/src/selection.py
+++ CgiDecode/src/selection.py
@@ -7,7 +7,6 @@ def selection(fitness_by_individual: dict, tournament_size: int) -> Tuple[str, f
     fitness_by_individual: Diccionario que contiene a los individuos de la población como keys y su fitness como valores.
     tournament_size: Tamaño del torneo (entero positivo).
     """
-    winner = None
-    # TODO: COMPLETAR
-    #  (Tournament selection)
+    individuals = sample(fitness_by_individual.keys(), tournament_size)
+    winner = max(individuals, key=lambda individual: fitness_by_individual[individual])
     return winner, fitness_by_individual[winner]
