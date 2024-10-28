b5a8ec9d6035a123eb32d06da78460130c08aac8
MatiasCra <cravchikm@gmail.com>
2024-10-28 17:24:29 -0300

    ej4

--- CgiDecode/src/get_fitness_cgi_decode.py
+++ CgiDecode/src/get_fitness_cgi_decode.py
@@ -1,6 +1,6 @@
 from typing import List
-
-from src.evaluate_condition import clear_maps
+from src.evaluate_condition import clear_maps, get_true_distance, get_false_distance
+from src.cgi_decode_instrumented import cgi_decode_instrumented
 
 
 def get_fitness_cgi_decode(test_suite: List[str]) -> float:
@@ -8,6 +8,27 @@ def get_fitness_cgi_decode(test_suite: List[str]) -> float:
     # Recuerden que los diccionarios true_distances y false_distances son globales
     clear_maps()
 
+    for test_case in test_suite:
+        try:
+            cgi_decode_instrumented(test_case)
+        except Exception:
+            pass
+
     fitness = 0
-    # TODO: COMPLETAR
-    return 0
+    for branch in range(1, 6):
+        dist = get_true_distance(branch)
+        if dist is None:
+            dist = 1
+        else:
+            dist /= dist + 1
+
+        fitness += dist
+        dist = get_false_distance(branch)
+        if dist is None:
+            dist = 1
+        else:
+            dist /= dist + 1
+
+        fitness += dist
+
+    return fitness
--- CgiDecode/test/test_get_fitness_cgi_decode.py
+++ CgiDecode/test/test_get_fitness_cgi_decode.py
@@ -5,8 +5,15 @@ from src.get_fitness_cgi_decode import get_fitness_cgi_decode
 
 class TestGetFitnessCgiDecode(unittest.TestCase):
     def testExample(self):
-        # TODO COMPLETAR
-        get_fitness_cgi_decode(["%AA"])
-        self.assertTrue(True)
-        self.assertFalse(False)
-        self.assertEqual(True, False)
+        self.assertEqual(get_fitness_cgi_decode([r"%AA"]), 2.357142857142857)
+        self.assertEqual(get_fitness_cgi_decode([r"\%AA"]), 1.8571428571428572)
+        self.assertEqual(get_fitness_cgi_decode([r"\%AU"]), 3.03021978021978)
+        self.assertEqual(get_fitness_cgi_decode([r"\%UU"]), 4.53021978021978)
+        self.assertEqual(get_fitness_cgi_decode([r"Hello+Reader"]), 4.972222222222222)
+        self.assertEqual(get_fitness_cgi_decode([r""]), 8.5)
+        self.assertEqual(get_fitness_cgi_decode([r"\%"]), 5.357142857142858)
+        self.assertEqual(get_fitness_cgi_decode([r"\%1"]), 5.523809523809524)
+        self.assertEqual(get_fitness_cgi_decode([r"+"]), 6.5)
+        self.assertEqual(get_fitness_cgi_decode([r"+\%1"]), 4.666666666666666)
+        self.assertEqual(get_fitness_cgi_decode([r"\%1+"]), 2.9404761904761907)
+        self.assertEqual(get_fitness_cgi_decode([r"\%1+", r"%+1", r"a+%AA"]), 0)
