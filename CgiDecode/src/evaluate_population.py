faf61f96fcbcb808c7e02045b0604c8a84294c03
MatiasCra <cravchikm@gmail.com>
2024-10-28 17:34:28 -0300

    ej6

--- CgiDecode/src/evaluate_population.py
+++ CgiDecode/src/evaluate_population.py
@@ -2,7 +2,6 @@ from typing import List
 
 from src.get_fitness_cgi_decode import get_fitness_cgi_decode
 
+
 def evaluate_population(population: List[List[str]]) -> dict:
-    fitness = {}
-    # TODO: COMPLETAR
-    return fitness
+    return {ind: get_fitness_cgi_decode(ind) for ind in population}
